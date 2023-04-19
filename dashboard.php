589f4c4a774da035d0fbb6eea7ac5623a1c7260f
sprata <tomasaslopes1@gmail.com>
2023-04-19 22:38:23 +0100

    help

--- dashboard.php
+++ dashboard.php
@@ -64,7 +64,7 @@
     <title>Plataforma IoT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="css/dashboardStyle.css">
-      <meta http-equiv="refresh" content="5;url=http://localhost/TI-MASTER/projecto/api/logenerator.php">
+      <meta http-equiv="refresh" content="5;url=http://localhost/projecto/projecto/api/logenerator.php">
 </head>
   <body class="body-park">
     <div class="container card-theme">
