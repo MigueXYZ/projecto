fa949f9091e9fae2ff05d909c9c4dae30d220550
sprata <tomasaslopes1@gmail.com>
2023-04-19 23:14:51 +0100

    logs a funcionar

--- api/logenerator.php
+++ api/logenerator.php
@@ -1,6 +1,6 @@
 <?php
 
-
+// Temperatura Log
 $valor_temperatura = file_get_contents("files/temperatura/valor.txt");
 // Set timezone
 date_default_timezone_set('WET');
@@ -8,12 +8,42 @@ date_default_timezone_set('WET');
 // Define file path
 $file_path = 'files/temperatura/log.txt';
 
-// Loop every 5 seconds
 $timestamp = date('Y/m/d H:i:s') . ';' . $valor_temperatura;
-echo $timestamp;
+
 // Append timestamp to file
 $file = fopen($file_path, 'a');
 fwrite($file, $timestamp . "\n");
 fclose($file);
+
+
+// Veiculo Log
+$veiculo_quant = file_get_contents("files/veiculo/quantidade.txt");
+
+// Define file path
+$file_path = 'files/veiculo/log.txt';
+
+$timestamp = date('Y/m/d H:i:s') . ';' . $veiculo_quant;
+
+// Append timestamp to file
+$file = fopen($file_path, 'a');
+fwrite($file, $timestamp . "\n");
+fclose($file);
+
+
+// Luzes Log
+
+$luzes_estado = file_get_contents("files/luzes/estado.txt");
+
+// Define file path
+$file_path = 'files/luzes/log.txt';
+
+$timestamp = date('Y/m/d H:i:s') . ';' . $luzes_estado;
+
+// Append timestamp to file
+$file = fopen($file_path, 'a');
+fwrite($file, $timestamp . "\n");
+fclose($file);
+
+
 
 header("Location: http://localhost/projecto/projecto/dashboard.php");
