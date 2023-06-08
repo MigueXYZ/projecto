8e4a940d34c739ef63776dddbfa15df94168f634
MigueXYZ <miguelito.gomes.silva@gmail.com>
2023-06-08 16:25:08 +0100

    added the python and fixed the url's just in case yk

---
+++ python/tirarfoto.py
@@ -0,0 +1,57 @@
+from importlib.metadata import files
+import cv2
+import sys
+import time
+import requests
+
+url= "http://localhost/projeto/projecto/upload.php"
+url1="http://localhost/projeto/projecto/api/api.php"
+url2="http://localhost/projeto/projecto/api/files/Fotos/tirar.txt"
+def tirarFoto():
+    camera = cv2.VideoCapture(0, cv2.CAP_DSHOW) # captura a imagem usando a camara
+    ret, image = camera.read()
+    print ("Resultado da Camera=" + str(ret)) #printa o resultado
+    cv2.imwrite('opencv_image.png', image) #escreve a imagem
+    camera.release() #solta a camara
+    cv2.destroyAllWindows() #destroi todas as janelas
+    return
+def sendFile(imagem):
+    import datetime
+    x = datetime.datetime.now()
+    ola ='../Camara/'+str(x.strftime("%Y_%m_%d-%H_%M_%S"))+'.png' #nome da imagem com data
+    r = requests.post(url, files={'imagem': (ola, open('opencv_image.png', 'rb') , 'image/jpeg')})
+    if r.status_code != 200:
+        print("ocorreu um erro")
+        print(r.status_code)
+    else:
+        print("POST com sucesso")
+    return
+try:
+    i=True
+    valor_dif = 0
+    while i:
+        parametros={"nome":"Veiculos"}
+        parametros2 = {"nome": "Fotos"}
+        r = requests.get("http://localhost/projeto/projecto/api/api.php?nome=Veiculos") #vai buscar os ficheiros a api
+        valor = r.text.strip()        
+        time.sleep(2)
+        l = requests.get(url2,parametros2)
+        l = l.text
+        print(valor)
+        if (float(valor)!=float(valor_dif)): #só tira a foto se os valores da porta forem 1 e -1
+            valor_dif = valor
+            print("Porta: "+valor)
+            tirarFoto() # tira a foto
+            img = cv2.imread('opencv_image.png', 0) #carrega a imagem de um ficheiro especifico e o devolve para a variavel img
+            cv2.destroyAllWindows() #destroi todas as janelas
+            sendFile(img) # manda a imagem
+            time.sleep(5)
+except KeyboardInterrupt: # caso haja interrupção de teclado CTRL+C
+    print( "Programa terminado pelo utilizador")
+
+except : # caso haja um erro qualquer
+
+    print( "Ocorreu um erro:", sys.exc_info() )
+
+finally : # executa sempre, independentemente se ocorreu exception
+    print( "Fim do programa")
---
+++ upload.php
@@ -0,0 +1,50 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "Recebido por um POST";
+    print_r($_POST);
+
+    $imagem = $_FILES['imagem'];
+    echo $imagem['name'];
+    var_dump($imagem);
+    $date = date('Y-m-d H:i:s');
+
+
+    if (isset($_FILES['imagem'])) {
+
+        echo file_put_contents("api/files/Fotos/valor.txt", $imagem['name']);
+        echo file_put_contents("api/files/Fotos/hora.txt", $date);
+        $data = $date . " " . $imagem['name']. PHP_EOL;
+        $data .= file_get_contents("api/files/Fotos/log.txt");
+        echo file_put_contents("api/files/Fotos/log.txt", $data);
+
+
+        $tmp_name = $_FILES['imagem']['tmp_name'];
+        $name = basename($_FILES['imagem']['name']);
+
+        move_uploaded_file($tmp_name, "imageLogs/".$imagem['name']);
+    } else {
+        echo "Parametros nao respeitados";
+        exit();
+    }
+
+} elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $valores = array(//transformação para um array
+        'nome' => file_get_contents("api/files/Fotos/nome.txt"),
+        'valor' => file_get_contents("api/files/Fotos/valor.txt"),
+        'hora' => file_get_contents("api/files/Fotos/hora.txt"),
+        'tirar' => file_get_contents("api/files/Fotos/tirar.txt"),
+        'log' => file_get_contents("api/files/Fotos//log.txt"),
+    );
+    echo json_encode($valores); //retorna uma string contendo a representação do json
+
+
+} else {
+    echo "Metodo nao permitido";
+    exit();
+}
+
+?>
