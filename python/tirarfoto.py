940b8ae02ca57a85e580f94988ecddd0512939ee
MigueXYZ <miguelito.gomes.silva@gmail.com>
2023-06-09 15:36:08 +0100

    Rearanged the furniture

--- python/tirarfoto.py
+++ python/tirarfoto.py
@@ -5,21 +5,19 @@ import time
 import requests
 
 url= "http://localhost/projeto/upload.php"
-url1="http://localhost/projeto/api/api.php"
-url2="http://localhost/projeto/api/files/Fotos/tirar.txt"
 def tirarFoto():
     camera = cv2.VideoCapture(0, cv2.CAP_DSHOW) # captura a imagem usando a camara
     ret, image = camera.read()
     print ("Resultado da Camera=" + str(ret)) #printa o resultado
     cv2.imwrite('opencv_image.png', image) #escreve a imagem
-    camera.release() #solta a camaraz
+    camera.release() #solta a camara
     cv2.destroyAllWindows() #destroi todas as janelas
     return
-def sendFile(imagem):
+def sendFile():
     import datetime
     x = datetime.datetime.now()
     ola ='../Camara/'+str(x.strftime("%Y_%m_%d-%H_%M_%S"))+'.png' #nome da imagem com data
-    r = requests.post(url, files={'imagem': (ola, open('opencv_image.png', 'rb') , 'image/jpeg')})
+    r = requests.post(url, files={'imagem': (ola, open('opencv_image.png', 'rb') , 'image/jpeg')}) #
     if r.status_code != 200:
         print("ocorreu um erro")
         print(r.status_code)
@@ -35,15 +33,14 @@ try:
         r = requests.get("http://localhost/projeto/api/api.php?nome=Veiculos") #vai buscar os ficheiros a api
         valor = r.text.strip()        
         time.sleep(2)
-
         print(valor)
         if (float(valor)!=float(valor_dif)): #só tira a foto se os valores forem diferentes
             valor_dif = valor
             print("Porta: "+valor)
             tirarFoto() # tira a foto
-            img = cv2.imread('opencv_image.png', 0) #carrega a imagem de um ficheiro especifico e o devolve para a variavel img
+            cv2.imread('opencv_image.png', 0) #carrega a imagem de um ficheiro especifico e o devolve para a variavel img
             cv2.destroyAllWindows() #destroi todas as janelas
-            sendFile(img) # manda a imagem
+            sendFile() # manda a imagem
         time.sleep(5)
 except KeyboardInterrupt: # caso haja interrupção de teclado CTRL+C
     print( "Programa terminado pelo utilizador")
--- upload.php
+++ upload.php
@@ -13,18 +13,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     if (isset($_FILES['imagem'])) {
+        if ($_FILES['imagem']['size'] > 1000000) {//se a imagem passar 1000KB
+            throw new RuntimeException('Exceeded filesize limit.');
+        }else{
+            $type=$_FILES[ 'imagem' ][ 'type' ];
+            $extensions=array( 'image/jpeg', 'image/png');
+            if( in_array( $type, $extensions )){ //se a imagem for png ou jpeg
+                 file_put_contents("api/files/Fotos/valor.txt", $imagem['name']);
+                 file_put_contents("api/files/Fotos/hora.txt", $date);
+                $data = $date . ";" . $imagem['name']. PHP_EOL;
+                $data .= file_get_contents("api/files/Fotos/log.txt");
+                 file_put_contents("api/files/Fotos/log.txt", $data);
 
-        echo file_put_contents("api/files/Fotos/valor.txt", $imagem['name']);
-        echo file_put_contents("api/files/Fotos/hora.txt", $date);
-        $data = $date . ";" . $imagem['name']. PHP_EOL;
-        $data .= file_get_contents("api/files/Fotos/log.txt");
-        echo file_put_contents("api/files/Fotos/log.txt", $data);
 
+                $tmp_name = $_FILES['imagem']['tmp_name'];
+                $name = basename($_FILES['imagem']['name']);
 
-        $tmp_name = $_FILES['imagem']['tmp_name'];
-        $name = basename($_FILES['imagem']['name']);
-
-        move_uploaded_file($tmp_name, "imageLogs/".$imagem['name']);
+                move_uploaded_file($tmp_name, "imageLogs/".$imagem['name']);
+            }
+        }
     } else {
         echo "Parametros nao respeitados";
         exit();
